e2066c7ce2e9d68a1a1c016e94b8cf770fc4a7e0
kaleeswaran02 <112927324+kaleeswaran02@users.noreply.github.com>
2022-11-17 23:03:02 +0530

    Update app.py

--- project development phase/Sprint_2/app.py
+++ project development phase/Sprint_2/app.py
@@ -20,9 +20,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'jfif'}
 
 
 conn = ibm_db.connect(
-    "DATABASE=bludb;HOSTNAME=ea286ace-86c7-4d5b-8580-3fbfa46b66.bs2io90l08b1od8lcg.databases.appdomain.cloud;PORT"
-    "=31509;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=xnc98967;PWD=wQioPYLq4Oanh",
-    '', '')
+    "DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32328;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=gmz37043;PWD=5eCfk6YOpNcZhK1H",'','')
 
 print(conn)
 
